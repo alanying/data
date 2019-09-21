4f828dffb90de14077fb73b75535e03ce14af611
alanying <46174760+alanying@users.noreply.github.com>
2019-09-22 00:27:59 +0100

    Add notes for Data Coercion

--- vector.r
+++ vector.r
@@ -28,3 +28,18 @@ codes[1:2]
 # If the entries of a vector are named, they may be accessed by referring to their name
 codes["canada"]
 codes[c("egypt","italy")]
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+Data Coerison
+
+Key Points
+In general, coercion is an attempt by R to be flexible with data types by guessing what was meant when an entry does not match the expected. For example, when defining x as
+>x <- c(1, "canada", 3)
+
+R coerced the data into characters. It guessed that because you put a character string in the vector, you meant the 1 and 3 to actually be character strings "1" and “3”.
+
+The functionas.character() turns numbers into characters.
+The function as.numeric() turns characters into numbers.
+In R, missing data is assigned the value NA.

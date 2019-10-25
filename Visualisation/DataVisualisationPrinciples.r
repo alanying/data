463d142103b16da6bb0b19633a4a37c327cff260
alanying <46174760+alanying@users.noreply.github.com>
2019-10-25 01:16:37 +0100

    Update DataVisualisationPrinciples.r

--- Visualisation/DataVisualisationPrinciples.r
+++ Visualisation/DataVisualisationPrinciples.r
@@ -224,3 +224,137 @@ Key points
 Encode a categorical third variable on a scatterplot using color hue or shape. Use the shape argument to control shape.
 Encode a continuous third variable on a using color intensity or size.
 
+
+########## case study: vaccines #########
+Textbook link
+This video corresponds to the textbook case study on vaccines. Information on color palettes can be found in the textbook section on encoding a third variable.
+
+Key points
+Vaccines save millions of lives, but misinformation has led some to question the safety of vaccines. The data support vaccines as safe and effective. We visualize data about measles incidence in order to demonstrate the impact of vaccination programs on disease rate.
+The RColorBrewer package offers several color palettes. Sequential color palettes are best suited for data that span from high to low. Diverging color palettes are best suited for data that are centered and diverge towards high or low values.
+The geom_tile geometry creates a grid of colored tiles.
+Position and length are stronger cues than color for numeric values, but color can be appropriate sometimes.
+Code: Tile plot of measles rate by year and state
+# import data and inspect
+library(tidyverse)
+library(dslabs)
+data(us_contagious_diseases)
+str(us_contagious_diseases)
+# assign dat to the per 10,000 rate of measles, removing Alaska and Hawaii and adjusting for weeks reporting
+the_disease <- "Measles"
+dat <- us_contagious_diseases %>%
+    filter(!state %in% c("Hawaii", "Alaska") & disease == the_disease) %>%
+    mutate(rate = count / population * 10000 * 52/weeks_reporting) %>%
+    mutate(state = reorder(state, rate))
+# plot disease rates per year in California
+dat %>% filter(state == "California" & !is.na(rate)) %>%
+    ggplot(aes(year, rate)) +
+    geom_line() +
+    ylab("Cases per 10,000") +
+    geom_vline(xintercept=1963, col = "blue")
+# tile plot of disease rate by state and year
+dat %>% ggplot(aes(year, state, fill=rate)) +
+    geom_tile(color = "grey50") +
+    scale_x_continuous(expand = c(0,0)) +
+    scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "Reds"), trans = "sqrt") +
+    geom_vline(xintercept = 1963, col = "blue") +
+    theme_minimal() + theme(panel.grid = element_blank()) +
+    ggtitle(the_disease) +
+    ylab("") +
+    xlab("")
+Code: Line plot of measles rate by year and state
+# compute US average measles rate by year
+avg <- us_contagious_diseases %>%
+    filter(disease == the_disease) %>% group_by(year) %>%
+    summarize(us_rate = sum(count, na.rm = TRUE)/sum(population, na.rm = TRUE)*10000)
+# make line plot of measles rate by year by state
+dat %>%
+    filter(!is.na(rate)) %>%
+    ggplot() +
+    geom_line(aes(year, rate, group = state), color = "grey50", 
+        show.legend = FALSE, alpha = 0.2, size = 1) +
+    geom_line(mapping = aes(year, us_rate), data = avg, size = 1, col = "black") +
+    scale_y_continuous(trans = "sqrt", breaks = c(5, 25, 125, 300)) +
+    ggtitle("Cases per 10,000 by state") +
+    xlab("") +
+    ylab("") +
+    geom_text(data = data.frame(x = 1955, y = 50),
+        mapping = aes(x, y, label = "US average"), color = "black") +
+    geom_vline(xintercept = 1963, col = "blue")
+
+########## Avoid Pseudo and Gratuitous 3D Plots ##########
+Textbook link
+This video corresponds to the textbook section on avoiding gratuitous 3D plots.
+[https://rafalab.github.io/dsbook/data-visualization-principles.html#avoid-pseudo-three-dimensional-plots]
+
+Key point
+In general, pseudo-3D plots and gratuitous 3D plots only add confusion. Use regular 2D plots instead.
+
+########## Avoid Too Many Significant Digits ##########
+Textbook link
+This video corresponds to the textbook section on avoiding too many significant digits.
+[https://rafalab.github.io/dsbook/data-visualization-principles.html#avoid-too-many-significant-digits]
+
+Key points
+In tables, avoid using too many significant digits. Too many digits can distract from the meaning of your data.
+Reduce the number of significant digits globally by setting an option. For example, options(digits = 3) will cause all future computations that session to have 3 significant digits.
+Reduce the number of digits locally using round or signif.
+
+########## Exercise 1: Tile plot - measles and smallpox ##########
+Modify the tile plot to show the rate of smallpox cases instead of measles cases.
+Exclude years in which cases were reported in fewer than 10 weeks from the plot.
+
+library(dplyr)
+library(ggplot2)
+library(RColorBrewer)
+library(dslabs)
+data(us_contagious_diseases)
+
+the_disease = "Smallpox"
+dat <- us_contagious_diseases %>% 
+   filter(!state%in%c("Hawaii","Alaska") & disease == the_disease & weeks_reporting > 9) %>% 
+   mutate(rate = count / population * 10000) %>% 
+   mutate(state = reorder(state, rate))
+
+dat %>% ggplot(aes(year, state, fill = rate)) + 
+  geom_tile(color = "grey50") + 
+  scale_x_continuous(expand=c(0,0)) + 
+  scale_fill_gradientn(colors = brewer.pal(9, "Reds"), trans = "sqrt") + 
+  theme_minimal() + 
+  theme(panel.grid = element_blank()) + 
+  ggtitle(the_disease) + 
+  ylab("") + 
+  xlab("")
+ 
+########## Exercise 2. Time series plot - measles and smallpox ##########
+Modify the sample code for the time series plot to plot data for smallpox instead of for measles.
+Once again, restrict the plot to years in which cases were reported in at least 10 weeks.
+
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+library(RColorBrewer)
+data(us_contagious_diseases)
+
+the_disease = "Smallpox"
+dat <- us_contagious_diseases %>%
+   filter(!state%in%c("Hawaii","Alaska") & disease == the_disease & weeks_reporting > 9) %>%
+   mutate(rate = count / population * 10000) %>%
+   mutate(state = reorder(state, rate))
+
+avg <- us_contagious_diseases %>%
+  filter(disease==the_disease) %>% group_by(year) %>%
+  summarize(us_rate = sum(count, na.rm=TRUE)/sum(population, na.rm=TRUE)*10000)
+
+dat %>% ggplot() +
+  geom_line(aes(year, rate, group = state),  color = "grey50", 
+            show.legend = FALSE, alpha = 0.2, size = 1) +
+  geom_line(mapping = aes(year, us_rate),  data = avg, size = 1, color = "black") +
+  scale_y_continuous(trans = "sqrt", breaks = c(5,25,125,300)) + 
+  ggtitle("Cases per 10,000 by state") + 
+  xlab("") + 
+  ylab("") +
+  geom_text(data = data.frame(x=1955, y=50), mapping = aes(x, y, label="US average"), color="black") + 
+  geom_vline(xintercept=1963, col = "blue")
+  
+
